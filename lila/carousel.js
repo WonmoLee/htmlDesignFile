ee7a8797562242c92c47d50aeb00844202e60177
wonmoLee <leewonmo94@gmail.com>
2020-12-11 23:08:32 +0900

    lila / 메인화면 메뉴바완료  및 캐러셀 구현중

--- lila/carousel.js
+++ lila/carousel.js
@@ -1,38 +1,18 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
-
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
-
-let counter = 1;
-const size = 900;
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-// Buttons
-nextBtn.addEventListener('click', ()=> {
-  if(counter >= carouselImages.length -1) return;
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter++;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-prevBtn.addEventListener('click', ()=> {
-  if(counter <= 0) return;
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter--;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-// Jump to First/Last Slide
-carouselSlide.addEventListener('transitionend', () => {
-  console.log(carouselImages[counter]);
-  if (carouselImages[counter].id === 'lastClone'){
-    carouselSlide.style.transition = 'none'; // 트랜지션 효과 없애기
-    counter = carouselImages.length -2; // couter 초기화
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'; // 실제 마지막 이미지로 이동.
-  } else if (carouselImages[counter].id === 'firstClone') {
-    carouselSlide.style.transition = 'none';
-    counter = carouselImages.length - counter; // couter 초기화
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+const SHOWING_CLASS = "showing";
+const firstSlide = document.querySelector(".slider_item:first-child");
+function slide() {
+  const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+  if(currentSlide) {
+    currentSlide.classList.remove(SHOWING_CLASS);
+    const nextSlide = currentSlide.nextElementSibling;
+    if(nextSlide) {
+      nextSlide.classList.add(SHOWING_CLASS);
+    } else {
+      firstSlide.classList.add(SHOWING_CLASS);
+    }
+  } else {
+    firstSlide.classList.add(SHOWING_CLASS);
   }
-});
+}
+slide();
+setInterval(slide, 6000);
