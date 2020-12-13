ddd92087673aeb2b166ff53fe9eb76cef8e8c058
wonmoLee <leewonmo94@gmail.com>
2020-12-13 15:36:44 +0900

    lila / 캐러셀 구현완료

--- lila/carousel.js
+++ lila/carousel.js
@@ -1,18 +1,46 @@
 const SHOWING_CLASS = "showing";
 const firstSlide = document.querySelector(".slider_item:first-child");
-function slide() {
+const lastSlide = document.querySelector(".slider_item:last-child");
+const progSize=document.getElementsByClassName('progs').length;
+var num = 0;
+var id;
+var move;
+
+//캐러셀 내 프로그레스바
+function progressBar(classNum) {
+  var ele=document.getElementsByClassName('progs')[classNum];
+  console.log(ele)
+  var width = -25;
+  id = setInterval(frame, 65);
+  function frame(){
+      if(width>=100) {
+        clearInterval(id);
+        setSlide('next');
+      } else {
+          width ++;
+          ele.style.width= (width < 0) ? "0%" : width + "%";
+      }
+  }
+}
+
+function setSlide(state) {
+  clearInterval(id);
   const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
   if(currentSlide) {
     currentSlide.classList.remove(SHOWING_CLASS);
-    const nextSlide = currentSlide.nextElementSibling;
-    if(nextSlide) {
-      nextSlide.classList.add(SHOWING_CLASS);
+    const stateSlide = (state == "next") ? currentSlide.nextElementSibling : currentSlide.previousElementSibling;
+    if(stateSlide){
+      stateSlide.classList.add(SHOWING_CLASS);
+      (state == "next") ? num++ : num --;
     } else {
-      firstSlide.classList.add(SHOWING_CLASS);
+      (state == "next") ? firstSlide.classList.add(SHOWING_CLASS) : lastSlide.classList.add(SHOWING_CLASS);
+      (state == "next") ? num = 0 : num = progSize - 1;
     }
   } else {
     firstSlide.classList.add(SHOWING_CLASS);
+    num = 0;
   }
+  progressBar(num);
 }
-slide();
-setInterval(slide, 6000);
+
+setSlide('next');
