3c414ca6c2a15b892d62f42bf1c77ed96024f1d6
wonmoLee <leewonmo94@gmail.com>
2020-12-15 20:48:27 +0900

    lila / 카운트다운 구현완료

--- lila/saleCount.js
+++ lila/saleCount.js
@@ -1,12 +1,12 @@
 function getTime() { 
     now = new Date(); 
-    dday = new Date(2022,12,15,18,00,00); 
-    // 원하는 날짜, 시간 정확하게 초단위까지 기입.
-    days = (dday - now) / 1000 / 60 / 60 / 24; 
+    dday = new Date(2023,12,15,14,00,00); 
+    console.log(dday - now);
+    days = (dday - now) / 1000 / 60 / 60 / 24;
     daysRound = Math.floor(days); 
     hours = (dday - now) / 1000 / 60 / 60 - (24 * daysRound); 
     hoursRound = Math.floor(hours); 
-    minutes = (dday - now) / 1000 /60 - (24 * 60 * daysRound) - (60 * hoursRound); 
+    minutes = (dday - now) / 1000 / 60 - (24 * 60 * daysRound) - (60 * hoursRound); 
     minutesRound = Math.floor(minutes); 
     seconds = (dday - now) / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) - (60 * minutesRound); 
     secondsRound = Math.round(seconds); 
@@ -16,5 +16,4 @@ function getTime() {
     document.getElementById("counter3").innerHTML = secondsRound; 
     newtime = window.setTimeout("getTime();", 1000); 
 } 
-
 getTime();
