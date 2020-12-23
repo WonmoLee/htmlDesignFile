2d4b036771c227d0007befdea91e2441eace6177
wonmoLee <leewonmo94@gmail.com>
2020-12-23 23:07:27 +0900

    lila / 최초모달 수정 및 혜택받기모달 구현중

--- lila/modal.js
+++ lila/modal.js
@@ -1,13 +1,28 @@
 'use strict';
 
-const modal = document.querySelector(".modal");
-const closeButton = modal.querySelector(".close_button");
-//동작함수
-const openModal = () => {
-    modal.classList.remove("hidden");
+//최초로 띄우는 모달
+const f_modal = document.querySelector(".firstModal");
+const f_closeButton = f_modal.querySelector(".f_closeButton");
+
+const f_openModal = () => {
+    f_modal.classList.remove("f_hidden");
 }
-const closeModal = () => {
-    modal.classList.add("hidden");
+const f_closeModal = () => {
+    f_modal.classList.add("f_hidden");
 }
-closeButton.addEventListener("click", closeModal);
-openModal();
+f_closeButton.addEventListener("click", f_closeModal);
+f_openModal();
+
+//메인 페이지 알림받기 모달
+const n_modal = document.querySelector(".noticeModal");
+const n_openButton = document.querySelector(".n_openButton");
+const n_closeButton = n_modal.querySelector(".n_closeButton");
+
+const n_openModal = () => {
+    n_modal.classList.remove("n_hidden");
+}
+const n_closeModal = () => {
+    n_modal.classList.add("n_hidden");
+}
+n_openButton.addEventListener("click", n_openModal);
+n_closeButton.addEventListener("click", n_closeModal);
